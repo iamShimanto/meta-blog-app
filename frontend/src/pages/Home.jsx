7654259ccdb65841a4c4fc14ccd269b518fa11c5
iamShimanto <shimantowebdeveloper@gmail.com>
2025-08-17 11:36:57 +0600

    news letter

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/home/Banner'
 import BlogList from '../components/blog/BlogList'
+import NewsLetter from '../components/home/NewsLetter'
 
 const Home = () => {
   return (
     <>
       <Banner />
-      <BlogList/>
+      <BlogList />
+      <NewsLetter/>
     </>
   )
 }
