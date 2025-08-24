1694b1162f7bfdbb36a0f8a26f0d93aafa047bf2
iamShimanto <shimantowebdeveloper@gmail.com>
2025-08-25 01:40:38 +0600

    blog model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI);
+}
+
+main()
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("server is running");
---
+++ backend/src/model/blogModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
