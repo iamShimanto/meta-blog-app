f5d91881cb551eea6ae992c35c814dd03be3a22d
iamShimanto <shimantowebdeveloper@gmail.com>
2025-08-27 00:10:43 +0600

    get all blogs

--- backend/src/routes/blogRoutes.js
+++ backend/src/routes/blogRoutes.js
@@ -3,19 +3,30 @@ const blogs = require("../model/blogModel");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This is blog page");
+router.get("/", async (req, res) => {
+  try {
+    const blog = await blogs.find().sort({ createdAt : -1});
+    res.status(200).send({ message: "Data fetched successfully", blog });
+  } catch (error) {
+    console.error("Error Data fetching", error);
+    res.status(500).send({ message: "Error Data fetching" });
+  }
 });
 
 router.post("/add-blog", async (req, res) => {
-  const newBlog = new blogs({
-    ...req.body,
-  });
-  const blog = await newBlog.save();
-  res.status(200).json({
-    message: "Blog created successfully",
-    blog,
-  });
+  try {
+    const newBlog = new blogs({
+      ...req.body,
+    });
+    const blog = await newBlog.save();
+    res.status(200).json({
+      message: "Blog created successfully",
+      blog,
+    });
+  } catch (error) {
+    console.error("Error creating a new blog", error);
+    res.status(500).send({ message: "Error creating a new blog" });
+  }
 });
 
 module.exports = router;
